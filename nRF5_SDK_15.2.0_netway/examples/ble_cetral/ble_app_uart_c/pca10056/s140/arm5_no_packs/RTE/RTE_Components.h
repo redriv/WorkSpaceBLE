095362bac6672d01ea30ecf6582b4bf6a2c059a7
username@email.com <3120001981@qq.com>
2022-01-06 22:36:58 +0800

    frist commit

---
+++ nRF5_SDK_15.2.0_netway/examples/ble_cetral/ble_app_uart_c/pca10056/s140/arm5_no_packs/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_uart_c_pca10056_s140' 
+ * Target:  'nrf52840_xxaa' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
